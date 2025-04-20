228e127c3afb9c2351b6fa5e4e67a8a9770ccafa
matt bee <72115097+mbluelander@users.noreply.github.com>
2025-04-20 11:03:03 -0400

    1.0.3
    Downloading english subtitles is now a configurable setting. Starting whirltube with a URL will automatically launch yt-dlp with the default settings.

--- wt.sh
+++ wt.sh
@@ -1,23 +1,45 @@
 #!/usr/bin/env bash
 #requires: yt-dlp, ffmpeg, VLC
 ans=0
-while [[ $ans -lt 1 || $ans -gt 3 ]]; do
+sub="[off]"
+
+if [[ $1 > "" ]]; then
+	ans=1
+	url=$1
+fi
+while [[ $ans -lt 1 || $ans -gt 4 ]]; do
 	clear
-	echo "whirltube 1.0.1" 
+	echo "whirltube 1.0.3" 
 	echo
 	echo "1. Download video"
 	echo "2. Extract Mp3"
 	echo "3. Play in VLC"
+	echo -n "4. Download subtitles? " && echo $sub
 	echo	
 	echo -n "Choice? "
 	read -n 1 ans
+	if [[ $ans -eq 4 && $sub == "[off]" ]]; then
+		sub="[on]"
+		ans=0
+	elif [[ $ans -eq 4 && $sub == "[on]" ]]; then
+		sub="[off]"
+		ans=0
+	fi
 done
+
 echo && echo
-echo -n "Enter URL: "
-read -r url
+while [[ $url = "" ]]; do
+	echo -n "Enter URL: "
+	read -r url
+done
 if [[ $ans -eq 1 ]]; then
-	yt-dlp $url -S vcodec:h264,fps,res:720,acodec:m4a --write-sub --write-auto-sub --sub-lang "en.*"
-	exit
+	if [[ $sub = "[on]" ]]; then
+		yt-dlp $url -S vcodec:h264,fps,res:720,acodec:m4a --write-sub --write-auto-sub --sub-lang "en.*"
+		exit
+	elif [[ $sub = "[off]" ]]; then
+		yt-dlp $url -S vcodec:h264,fps,res:720,acodec:m4a
+		exit
+	fi
 elif [[ $ans -eq 2 ]]; then
 	yt-dlp --audio-format mp3 $url -x
 	exit
