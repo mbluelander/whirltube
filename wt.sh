76c2633d8eba9d74606d1ffb036e055c674d05b8
matt bee <72115097+mbluelander@users.noreply.github.com>
2025-04-19 23:11:04 -0400

    1.0.1
    whirltube will now download any available english subtitles for video.

--- wt.sh
+++ wt.sh
@@ -3,7 +3,7 @@
 ans=0
 while [[ $ans -lt 1 || $ans -gt 3 ]]; do
 	clear
-	echo "whirltube 1.0" 
+	echo "whirltube 1.0.1" 
 	echo
 	echo "1. Download video"
 	echo "2. Extract Mp3"
@@ -16,7 +16,7 @@ echo && echo
 echo -n "Enter URL: "
 read -r url
 if [[ $ans -eq 1 ]]; then
-	yt-dlp $url -S vcodec:h264,fps,res:720,acodec:m4a
+	yt-dlp $url -S vcodec:h264,fps,res:720,acodec:m4a --write-sub --write-auto-sub --sub-lang "en.*"
 	exit
 elif [[ $ans -eq 2 ]]; then
 	yt-dlp --audio-format mp3 $url -x
