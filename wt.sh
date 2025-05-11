43fe80c7ebfba062a427864654c1464fd19fc2b9
matt bee <72115097+mbluelander@users.noreply.github.com>
2025-05-10 20:49:29 -0400

    Update wt.sh
    1.1.4

--- wt.sh
+++ wt.sh
@@ -1,25 +1,36 @@
 #!/usr/bin/env bash
 #requires: yt-dlp, ffmpeg, VLC
 
-#https://github.com/mbluelander/whirltube/
-version="🌀 whirltube 1.1.3"
+#https://mattbee.zone/whirltube
+version="🌀 whirltube 1.1.4"
 
-### DEFAULT SETTINGS
-sub=0		# 1 = download subtitles of specified language
+### DEFAULT SETTINGS ################################################
+sub=1		# 1 = download subtitles of specified language
 lang="en-en" 	# Subtitle language
 res=720 	# Default resolution. 480, 720, 1080, etc.
-form="Mp3" 	# Format for audio extraction. Mp3, Opus, M4a, etc.
+form="mp3" 	# Format for audio extraction. Mp3, Opus, M4a, etc.
 cookies=0	# 1 = import browser cookies for login-gated content
-bsr="firefox"	# Browser from which to allow cookies				
-###
+bsr="firefox"	# Browser from which to allow cookies
+embed=0		# 1 = embed chapter metadata
+### ADVANCED ########################################################
+vc="h264"	# Video codec
+ac="m4a"	# Audio codec
+player="vlc"	# Local video player for streaming
+#####################################################################
 
 ans=0
 subu=("[x] no    [ ] yes" "[ ] no    [x] yes")
 cookiesu=("[x] no    [ ] yes" "[ ] no    [x] yes")
+
 if [[ cookies -eq 1 ]]; then
 	usecookies="--cookies-from-browser $bsr"
 else
 	usecookies=""
+fi
+if [[ embed -eq 1 ]]; then
+	chapters="--embed-chapters"
+else
+	chapters=""
 fi
 
 if [[ $1 > "" ]]; then
@@ -32,7 +43,7 @@ while [[ $ans -lt 1 || $ans -gt 6 ]]; do
 	echo
 	echo " 1. Download video" 
 	echo " 2. Extract audio"
-	echo " 3. Play in VLC"
+	echo " 3. Play in $player"
 	echo -n " 4. Download subtitles? " && echo "${subu[sub]}"
 	echo -n " 5. Use cookies?        " && echo "${cookiesu[cookies]}"
 	echo -n " 6. Change resolution   [" && echo -n $res && echo "p]"
@@ -45,6 +56,7 @@ while [[ $ans -lt 1 || $ans -gt 6 ]]; do
 		echo && echo
 		exit
 	fi
+		
 	if [[ $ans -eq 4 && $sub -eq 0 ]]; then
 		sub=1
 		ans=0
@@ -71,13 +83,13 @@ while [[ $ans -lt 1 || $ans -gt 6 ]]; do
 		res=720
 		ans=0
 	fi
-	if [[ $ans -eq 7 && $form == "Mp3" ]]; then
+	if [[ $ans -eq 7 && $form == "mp3" ]]; then
 		form="Opus"
 		ans=0
-	elif [[ $ans -eq 7 && $form == "Opus" ]]; then
+	elif [[ $ans -eq 7 && $form == "opus" ]]; then
 		form="M4a"
 		ans=0
-	elif [[ $ans -eq 7 && $form == "M4a" ]]; then
+	elif [[ $ans -eq 7 && $form == "m4a" ]]; then
 		form="Mp3"
 		ans=0
 	fi
@@ -90,17 +102,17 @@ while [[ $url == "" ]]; do
 done
 if [[ $ans -eq 1 ]]; then
 	if [[ $sub -eq 1 ]]; then
-		yt-dlp $usecookies $url -S vcodec:h264,fps,res:$res,acodec:m4a --write-sub --write-auto-sub --sub-lang "$lang.*" --embed-chapters
+		yt-dlp $usecookies $url -S vcodec:$vc,fps,res:$res,acodec:$ac --write-sub --write-auto-sub --sub-lang "$lang.*" $chapters
 		exit
 	else
-		yt-dlp $usecookies $url -S vcodec:h264,fps,res:$res,acodec:m4a --embed-chapters
+		yt-dlp $usecookies $url -S vcodec:$vc,fps,res:$res,acodec:$ac $chapters
 		exit
 	fi
 elif [[ $ans -eq 2 ]]; then 
 	yt-dlp $usecookies --audio-format $form $url -x
 	exit
 elif [[ $ans -eq 3 ]]; then
-	yt-dlp $usecookies -o - --downloader ffmpeg -f "bv*+ba/b" - $url | vlc -
+	yt-dlp $usecookies -o - --downloader ffmpeg -f "bv*+ba/b" - $url | $player -
 	exit
 fi
 exec bash
